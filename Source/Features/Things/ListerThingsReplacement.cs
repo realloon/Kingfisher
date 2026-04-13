7dfb5b79c93bd970510d94117d3880ef955da1b8
sincerajn <sincerajn@outlook.com>
2026-04-14 03:00:59 +0800

    perf: tail-optimize lister things removal

--- Source/Features/Things/ListerThingsReplacement.cs
+++ Source/Features/Things/ListerThingsReplacement.cs
@@ -1,24 +1,17 @@
 namespace Kingfisher.Features.Things;
 
 internal static class ListerThingsReplacement {
-    private static readonly ThingRequestGroup[] AllGroups = ThingListGroupHelper.AllGroups;
-
     public static void Remove(ListerThings listerThings, Thing thing) {
         if (!ListerThings.EverListable(thing.def, listerThings.use)) {
             return;
         }
 
-        if (thing.def.projectile != null) {
-            RemoveProjectile(listerThings, thing);
-            return;
-        }
-
         if (listerThings.listsByDef.TryGetValue(thing.def, out var byDefList)) {
-            byDefList.Remove(thing);
+            RemoveFromTail(byDefList, thing);
         }
 
         if (thing is IHaulSource haulSource) {
-            listerThings.haulSources.Remove(haulSource);
+            RemoveFromTail(listerThings.haulSources, haulSource);
         }
 
         for (var i = 0; i < AllGroups.Length; i++) {
@@ -31,56 +24,26 @@ internal static class ListerThingsReplacement {
                 continue;
             }
 
-            listerThings.listsByGroup[i].Remove(thing);
+            RemoveFromTail(listerThings.listsByGroup[i], thing);
             listerThings.stateHashByGroup[(int)group] += 1;
         }
 
         listerThings.thingListChangedCallbacks?.onThingRemoved?.Invoke(thing);
     }
 
-    private static void RemoveProjectile(ListerThings listerThings, Thing thing) {
-        if (listerThings.listsByDef.TryGetValue(thing.def, out var byDefList)) {
-            RemoveRecentThing(byDefList, thing);
-        }
+    # region Helper
 
-        if (thing is IHaulSource haulSource) {
-            RemoveRecentHaulSource(listerThings.haulSources, haulSource);
-        }
+    private static readonly ThingRequestGroup[] AllGroups = ThingListGroupHelper.AllGroups;
 
-        for (var i = 0; i < AllGroups.Length; i++) {
-            var group = AllGroups[i];
-            if (listerThings.use == ListerThingsUse.Region && !group.StoreInRegion()) {
-                continue;
-            }
-
-            if (!group.Includes(thing.def)) {
-                continue;
-            }
-
-            RemoveRecentThing(listerThings.listsByGroup[i], thing);
-            listerThings.stateHashByGroup[(int)group] += 1;
-        }
-
-        listerThings.thingListChangedCallbacks?.onThingRemoved?.Invoke(thing);
-    }
-
-    private static void RemoveRecentThing(List<Thing> list, Thing thing) {
-        var index = list.LastIndexOf(thing);
+    private static void RemoveFromTail<T>(List<T> list, T item) {
+        var index = list.LastIndexOf(item);
         if (index >= 0) {
             list.RemoveAt(index);
             return;
         }
 
-        list.Remove(thing);
+        list.Remove(item);
     }
 
-    private static void RemoveRecentHaulSource(List<IHaulSource> list, IHaulSource haulSource) {
-        var index = list.LastIndexOf(haulSource);
-        if (index >= 0) {
-            list.RemoveAt(index);
-            return;
-        }
-
-        list.Remove(haulSource);
-    }
+    # endregion
 }
