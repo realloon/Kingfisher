ed4ac5c7ed994c286b8f62dfa1b25a460dc0d2af
sincerajn <sincerajn@outlook.com>
2026-04-18 05:46:38 +0800

    chore: define mixed licensing

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Kingfisher
 
 Kingfisher is a performance optimization mod for RimWorld.
+
+## License
+
+This repository uses mixed licensing. See [LICENSE.md](LICENSE.md) for the file-level split between MIT and MPL-2.0.
--- Source/Features/Buildings/ListerBuildingsAddPatch.cs
+++ Source/Features/Buildings/ListerBuildingsAddPatch.cs
@@ -1,3 +1,10 @@
+// Copyright (c) 2021 bradson
+// Copyright (c) 2026 Vortex
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+
 using JetBrains.Annotations;
 using HarmonyLib;
 
